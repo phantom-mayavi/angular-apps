35e3ab0555add05c1ec5fed37d1ca0bb05336ca5
phantom-mayavi <phantom.mayavi@gmail.com>
2019-05-28 23:26:26 +0800

    added event emiter

--- cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Test server', content: 'sample content'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
