97fee10aa76376911fd4d63e258df1aafedcd332
phantom-mayavi <phantom.mayavi@gmail.com>
2019-06-20 00:39:30 +0800

    changes

--- services-assignment-start/src/app/app.module.ts
+++ services-assignment-start/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { CounterService } from './counter.service';
     BrowserModule,
     FormsModule,
   ],
-  providers: [UsersService, CounterService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- services-assignment-start/src/app/counter.service.ts
+++ services-assignment-start/src/app/counter.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
 export class CounterService {
     private inactiveToActiveCtr: number = 0;
     private activeToInactiveCtr: number = 0;
