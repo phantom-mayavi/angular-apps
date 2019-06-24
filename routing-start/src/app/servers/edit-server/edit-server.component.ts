6af4bd6850eaa1e586d2c1b52c93028ec185fdcc
phantom-mayavi <phantom.mayavi@gmail.com>
2019-06-25 07:05:51 +0800

    passing query params

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 @Component({
@@ -13,6 +13,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit: boolean = false;
 
   constructor(private serversService: ServersService,
     private route: ActivatedRoute) { }
@@ -21,10 +22,13 @@ export class EditServerComponent implements OnInit {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
-    console.log("query params " + this.route.snapshot.queryParams['allowEdit']);
-    
-    
+    console.log("query params ", this.route.snapshot.queryParams);
     console.log("fragment  " + this.route.snapshot.fragment);
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    );
   }
 
   onUpdateServer() {
