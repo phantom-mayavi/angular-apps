262821631a4d19e611566a796725077925b61188
phantom-mayavi <phantom.mayavi@gmail.com>
2019-07-04 09:17:37 +0800

    routing

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -41,6 +41,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
+        // RouterModule.forRoot(appRoutes, {useHash: true})
         RouterModule.forRoot(appRoutes)
     ],
     exports: [
